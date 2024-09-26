edebd71a2b17e98f1f4d4cb6c7b574c5142e626e
Nosoead <thseodh@gmail.com>
2024-09-26 15:55:26 +0900

    [Fix]_IOManager_Son
    IOManager 다시만들고
    Monster -> public 했습니다.

--- Team2/2GETHER/IOManager.cs
+++ Team2/2GETHER/IOManager.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _2GETHER
+﻿namespace _2GETHER
 {
-    internal class Class1
+    class IOManager
     {
+
     }
 }
