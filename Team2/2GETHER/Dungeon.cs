fc4628fb64710970e111932d507ed5db4b000ba5
hoshi <chlehdrb4967@gmail.com>
2024-09-26 20:03:12 +0900

    [Update]_Dungeon_Choi

--- Team2/2GETHER/Dungeon.cs
+++ Team2/2GETHER/Dungeon.cs
@@ -1,7 +1,19 @@
 ﻿namespace _2GETHER
 {
     public class Dungeon
-    {
+    {   
+        public void CreateMonster()
+        {
 
+        }
+        public void StartBattle()
+        {
+            Monster mosnter= new Monster();
+            Player player= new Player();
+            Console.WriteLine($"전투 시작~! 플레이어 정보: 체력({player.hp}), 공격력({player.attack})");
+            
+
+        }
+        
     }
 }
