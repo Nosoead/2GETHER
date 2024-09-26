f7a11fe2086437da2f946d58ec8e6687b8a0b6d0
GammaTT <52464999+GammaTT@users.noreply.github.com>
2024-09-26 19:57:31 +0900

    [Fix]_IOManager&Program_Yi
    IO 매니저 기본적인 메시지 출력 기능
    게임 매니저 기본적인 게임 실행과
    메뉴 출력 테스트

--- Team2/2GETHER/Program.cs
+++ Team2/2GETHER/Program.cs
@@ -4,7 +4,86 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GameManager gameManager = new GameManager();
+
+            gameManager.GameStart();
+        }
+    }
+
+    public class GameManager
+    {
+        IOManager ioManager = new IOManager();
+
+        public void GameStart()
+        {
+            ioManager.PrintMessage(ioManager.GameStartSceneMessage);
+            ioManager.PlzInputAnyKey();
+            Game();
+        }
+
+        public void Game()
+        {
+            int select;
+
+            while (true)
+            {
+                select = ioManager.PrintMessageWithNumberForSelect(ioManager.MenuSceneMessage);
+
+                Action(select);
+            }
+        }
+
+        public void Action(int select)
+        {
+            switch (select)
+            {
+                //상태보기
+                case 1:
+                    Status();
+                    break;
+
+                //인벤토리
+                case 2:
+                    Inventory();
+                    break;
+
+                //상점
+                case 3:
+                    Store();
+                    break;
+
+                //던전 입장
+                case 4:
+                    Dungeon();
+                    break;
+
+                //임시 테스트용
+                case 5:
+                    ioManager.PrintDebugMessage();
+                    break;
+            }
+
+            return;
+        }
+
+        public void Status()
+        {
+
+        }
+
+        public void Inventory()
+        {
+
+        }
+
+        public void Store()
+        {
+
+        }
+
+        public void Dungeon()
+        {
+
         }
     }
 }
