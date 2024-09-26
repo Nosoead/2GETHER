37d28d7b64a37e1fd35535a523afcf4cee913020
kkamsogod <9310rlawlgns@gmail.com>
2024-09-26 20:05:31 +0900

    [Update]_Class_Kim
    플레이어, 몬스터클래스 업데이트

--- Team2/2GETHER/Monster.cs
+++ Team2/2GETHER/Monster.cs
@@ -1,7 +1,25 @@
-﻿namespace _2GETHER
+﻿using System;
+using System.Reflection.Emit;
+
+namespace _2GETHER
 {
     public class Monster
     {
+        EMonsterName name;
+        int level;
+        int hp;
+        int attack;
 
+
+        
+        public void printMonsterInfo()
+        {
+            Console.WriteLine($"Lv: {level}, {name}, HP: {hp}, ATK: {attack}");
+        }
     }
+
+    public enum EMonsterName { 고블린, 오크, 오우거, 고블린킹 }
+
+    
+
 }
