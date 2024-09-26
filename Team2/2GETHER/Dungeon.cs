79f4f1069475767f01f04736b93a2921cf585928
hoshi <chlehdrb4967@gmail.com>
2024-09-26 20:48:23 +0900

    [Update_Dungeon_Choi]
    약간의 수정이 있습니다.

--- Team2/2GETHER/Dungeon.cs
+++ Team2/2GETHER/Dungeon.cs
@@ -4,6 +4,9 @@
     {   
         public void CreateMonster()
         {
+            EMonsterName eMonsterName = new EMonsterName();
+            Random random = new Random();
+
 
         }
         public void StartBattle()
@@ -11,6 +14,7 @@
             Monster mosnter= new Monster();
             Player player= new Player();
             Console.WriteLine($"전투 시작~! 플레이어 정보: 체력({player.hp}), 공격력({player.attack})");
+
             
 
         }
