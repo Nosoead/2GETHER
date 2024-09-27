8b925d8bbe34b8eb82e1281e41b3987ea37325d3
hoshi <chlehdrb4967@gmail.com>
2024-09-27 10:34:55 +0900

    [Update]_Class_Choi

--- Team2/2GETHER/Dungeon.cs
+++ Team2/2GETHER/Dungeon.cs
@@ -7,29 +7,48 @@ namespace _2GETHER
 
     public class Dungeon
     {
+        private List<Monster> monsters = new List<Monster>();
+
         public void CreateMonster()
         {
             Random random = new Random();
             int monsterCount = random.Next(1, 5);
-            List<EMonsterName> monsters = new List<EMonsterName>();
+
             for (int i = 0; i < monsterCount; i++)
             {
-                EMonsterName randomMonster = (EMonsterName)random.Next(Enum.GetValues(typeof(EMonsterName)).Length);
-                monsters.Add(randomMonster);
+                Monster newMonster = GenerateRandomMonster(random.Next(1, 5));
+                monsters.Add(newMonster);
             }
-            foreach (EMonsterName monster in monsters)
+        }
+
+        private Monster GenerateRandomMonster(int monsterType)
+        {
+            switch (monsterType)
             {
-                Console.WriteLine(monster);
+                case 1:
+                    return new Goblin();
+                case 2:
+                    return new Oak();
+                case 3:
+                    return new Ooger();
+                case 4:
+                    return new GoblinKing();
+                default:
+                    throw new ArgumentException("유효하지 않은 몬스터 타입입니다.");
             }
         }
+
         public void StartBattle()
-        { 
-            Console.WriteLine($"전투 시작~!");
-            CreateMonster();          
+        {
+            Console.WriteLine("전투 시작!");
+            CreateMonster();
+
+            foreach (Monster monster in monsters)
+            {
+                Console.WriteLine(monster.GetMonsterInfo());
+            }
         }
-
-
-    }    
+    }
 }
 
 
--- Team2/2GETHER/Monster.cs
+++ Team2/2GETHER/Monster.cs
@@ -2,39 +2,42 @@
 {
     public class Monster
     {
-        public EMonsterName name;
+        public string name;
         public int level;
         public int hp;
-        public int attack;
+        public double attack;
 
-        public Monster()
+        public Monster(string name, int level, int hp, double attack)
         {
-            name = EMonsterName.고블린;
-            level = 1;
-            hp = 50;
-            attack = 10;
-        }
-
-
-
-
-        public void MonsterAttack()
-
-        // 몬스터 공격, 죽었을때 데미지 안들어가고 안때리고(bool값 이용)
-        {
-
-
+            this.name = name;
+            this.level = level;
+            this.hp = hp;
+            this.attack = attack;
         }
 
         public string GetMonsterInfo()
         {
-            string monsterInfo = $"Lv: {level}, {name}, HP: {hp}, ATK: {attack}";
-            return monsterInfo;
+            return $"Lv: {level}, {name}, HP: {hp}, ATK: {attack}";
         }
     }
 
-    public enum EMonsterName { 고블린, 오크, 오우거, 고블린킹 }
+    public class Goblin : Monster
+    {
+        public Goblin() : base("고블린", 1, 50, 10) { }
+    }
 
+    public class Oak : Monster
+    {
+        public Oak() : base("오크", 2, 70, 15) { }
+    }
 
+    public class Ooger : Monster
+    {
+        public Ooger() : base("오우거", 3, 90, 20) { }
+    }
 
+    public class GoblinKing : Monster
+    {
+        public GoblinKing() : base("고블린킹", 5, 120, 25) { }
+    }
 }
