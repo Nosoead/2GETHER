2326de605d3c5155de63aa22cdce41a332093219
hoshi <chlehdrb4967@gmail.com>
2024-09-27 19:09:07 +0900

    [Update]_Dungeon_Choi
    로직 수정

--- Team2/2GETHER/Dungeon.cs
+++ Team2/2GETHER/Dungeon.cs
@@ -1,11 +1,12 @@
 ﻿namespace _2GETHER
 {
     class Dungeon
-    {
+    {   
         IOManager iomanager = new IOManager();
         private List<Monster> monsters = new List<Monster>();
         public void CreateMonster()
-        {
+        {   
+            monsters.Clear();
             Random random = new Random();
             int monsterCount = random.Next(1, 5);
             for (int i = 0; i < monsterCount; i++)
@@ -13,6 +14,7 @@
                 Monster newMonster = GenerateRandomMonster(random.Next(1, 5));
                 monsters.Add(newMonster);
             }
+
 
         }
         private Monster GenerateRandomMonster(int monsterType)
@@ -34,7 +36,7 @@
         public void StartBattle(Player player)
         {
             CreateMonster();
-            int choice;
+            int select;
             string[] monsterMessages = new string[monsters.Count];
             for (int i = 0; i < monsters.Count; i++)
             {
@@ -43,37 +45,25 @@
             iomanager.PrintMessage("전투 시작~!\n", true);
             iomanager.PrintMessage(monsterMessages, false);
             
-            string[] str =
+            string[] battleInfo =
             {   
                 "",
                 "[내정보]",
                 "",
                 $"Lv.{player.Level}  {player.Name}  ({player.Job})",
-                $"HP {player.Hp}/100",
-                "원하는 행동을 입력해 주세요."
+                $"HP {player.Hp}/100"
             };
-            iomanager.PrintMessage(str, false);
+            iomanager.PrintMessage(battleInfo, false);
 
             string[] atkchoice = {"공격", "도망가기"};
-            iomanager.PrintMessageWithNumberForSelect(atkchoice, false);
+            select=iomanager.PrintMessageWithNumberForSelect(atkchoice, false);
+            if(select == 1)
+            {
 
-            for (int i = 0; i < monsters.Count; i++)
-            {
-                monsterMessages[i] = monsters[i].GetMonsterInfo();
-            }
-            iomanager.PrintMessageWithNumberForSelect(monsterMessages, false);
-
-            while (!int.TryParse(Console.ReadLine(), out choice) || (choice < 0 || choice > 1))
-            {
-                Console.WriteLine("잘못된 입력입니다. 다시 입력해주세요.\n");
-            }
-            if (choice == 0)
-            {
-                //도망갈때 나오는 대사 입력
             }
             else
             {
-                //플레이어가 공격하는 턴
+                
             }
         }
     }
