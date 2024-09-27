63ef2ae67a27355c38b25784fc863725383e88f0
kkamsogod <9310rlawlgns@gmail.com>
2024-09-27 10:35:40 +0900

    [Update]_Status_Kim
    스테이터스 클래스 새로 만듬

---
+++ Team2/2GETHER/Status.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection.Emit;
+using System.Xml.Linq;
+
+namespace _2GETHER
+{
+    class Status
+    {
+        public string GetStatusInfo(Player player)
+        {
+            
+            string[] statusinfo = new string[]
+            {
+                "상태 보기",
+                "캐릭터의 정보가 표시됩니다.",
+                "\n",
+                $"{player.name} ({player.job})",
+                "",
+                "",
+                "",
+                "",
+
+
+
+
+
+
+
+
+
+
+            };
+
+            return string.Join(",", statusinfo);
+        }
+    }
+}
