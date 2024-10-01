87a7f9337d22501da068bd541dfe96a3444e579d
Nosoead <thseodh@gmail.com>
2024-10-01 16:17:03 +0900

    [Update]_Inventory, Item, ItemManager, Store_Son
    Store 클래스에 있던
    Item, Item Manager, Inventory, 나누고 추가 작업 했습니다.

---
+++ Team2/2GETHER/Inventory.cs
@@ -0,0 +1,135 @@
+﻿namespace _2GETHER
+{
+    class Inventory
+    {
+        public void EnterInventory(Player player, IOManager ioManager, ItemManager itemManager)
+        {
+            int selectNumber = -1;
+            int inputNum = 0;
+            do
+            {
+                ioManager.PrintMessage(ShowInventoryWindow(player), true);
+                //그냥 상점창
+                if (selectNumber == -1)
+                {
+                    ioManager.PrintMessage(GetInventoryItemList(player), false);
+                    selectNumber = ioManager.PrintMessageWithNumberForSelectZeroExit(ShowInventoryMenu(), false);
+                }
+                //구매하기 상점창
+                else if (selectNumber == 1)
+                {
+                    inputNum = ioManager.PrintMessageWithNumberForSelectZeroExit(GetInventoryItemList(player), false);
+                    selectNumber = ItemEquipment(inputNum, player, itemManager);
+                }
+            }
+            while (selectNumber != 0);
+        }
+        public string[] ShowInventoryWindow(Player player)
+        {
+            List<string> storeList = new List<string>()
+            {
+                "인벤토리",
+                "보유 중인 아이템을 관리할 수 있습니다.\n",
+                "[아이템 목록]"
+            };
+            return storeList.ToArray();
+        }
+
+        public string[] ShowInventoryMenu()
+        {
+            List<string> menuList = new List<string>()
+            {
+                "장착 관리"
+            };
+            return menuList.ToArray();
+        }
+        public string[] GetInventoryItemList(Player player)
+        {
+            List<string> inventoryItemList = new List<string>();
+            foreach (var equipmentItem in player.equipmentInventory)
+            {
+                inventoryItemList.Add(equipmentItem.GetItemInfo(EItemDo.ItemView));
+            }
+            return inventoryItemList.ToArray();
+        }
+
+        public int ItemEquipment(int inputNum, Player player, ItemManager itemManager)
+        {
+            if (inputNum == 0) return -1;
+            else
+            {
+                // EquipItem(inputNum, player);
+                Item selectItem = player.equipmentInventory[inputNum - 1];
+                if (selectItem.eItemType==EItemType.Armor)
+                {
+                    EquipArmor(inputNum, player);
+                }
+                else if (selectItem.eItemType == EItemType.Weapon)
+                {
+                    EquipWeapon(inputNum, player);
+                }
+            }
+            return 1;
+        }
+
+        public void EquipArmor(int inputNum, Player player)
+        {
+            if (player.ArmorEquipment[0] == null)
+            {
+                player.equipmentInventory[inputNum - 1].IsPlayerEquip = true;
+                player.ArmorEquipment[0] = player.equipmentInventory[inputNum - 1];
+            }
+            else
+            {
+
+                if (player.ArmorEquipment[0].eItem == player.equipmentInventory[inputNum - 1].eItem)
+                {
+                    player.ArmorEquipment[0] = null;
+                    player.equipmentInventory[inputNum - 1].IsPlayerEquip = false;
+                }
+                else
+                {
+                    for (int i = 0; i < player.equipmentInventory.Count; i++)
+                    {
+                        if (player.ArmorEquipment[0].eItem == player.equipmentInventory[i].eItem)
+                        {
+                            player.equipmentInventory[i].IsPlayerEquip = false;
+                            player.equipmentInventory[inputNum - 1].IsPlayerEquip = true;
+                            player.ArmorEquipment[0] = player.equipmentInventory[inputNum - 1];
+                        }
+                    }
+                }
+            }
+        }
+        public void EquipWeapon(int inputNum, Player player)
+        {
+            if (player.WeaponEquipment[0] == null)
+            {
+                player.equipmentInventory[inputNum - 1].IsPlayerEquip = true;
+                player.WeaponEquipment[0] = player.equipmentInventory[inputNum - 1];
+            }
+            else
+            {
+
+                if (player.WeaponEquipment[0].eItem == player.equipmentInventory[inputNum - 1].eItem)
+                {
+                    player.WeaponEquipment[0] = null;
+                    player.equipmentInventory[inputNum - 1].IsPlayerEquip = false;
+                }
+                else
+                {
+                    for (int i = 0; i < player.equipmentInventory.Count; i++)
+                    {
+                        if (player.WeaponEquipment[0].eItem == player.equipmentInventory[i].eItem)
+                        {
+                            player.equipmentInventory[i].IsPlayerEquip = false;
+                            player.equipmentInventory[inputNum - 1].IsPlayerEquip = true;
+                            player.WeaponEquipment[0] = player.equipmentInventory[inputNum - 1];
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Team2/2GETHER/Item.cs
@@ -0,0 +1,201 @@
+﻿using System.Diagnostics;
+
+namespace _2GETHER
+{
+    public enum EItem
+    {
+        누더기_갑옷 = 1,
+        구리갑옷,
+        르탄의_갑옷,
+        나뭇가지,
+        청동_거울,
+        르탄의_창,
+        빨간_포션,
+
+        낡은_검 = 101,
+        강철_검,
+        청동_도끼,
+        엑스칼리버,
+        수련자_갑옷,
+        무쇠_갑옷,
+        스파르타_갑옷,
+
+        낡은_석궁 = 201,
+        강철_활,
+        다이아몬드_활,
+        아스트라,
+        수련자_가죽갑옷,
+        무쇠_가죽갑옷,
+        스파르타_가죽갑옷,
+
+        낡은_지팡이 = 301,
+        강철_지팡이,
+        마법_지팡이,
+        아티쉬,
+        수련자_로브,
+        무쇠_로브,
+        스파르타_로브
+    }
+
+    public enum EItemType
+    {
+        Armor = 1,
+        Weapon,
+        Potion
+    }
+
+    public enum EItemDo
+    {
+        ItemBuy,
+        ItemSell,
+        ItemView
+    }
+    class Item
+    {
+        public EItem eItem { get; protected set; }
+        public EItemType eItemType { get; protected set; }
+        public int Price { get; protected set; }
+        public int ItemATK { get; protected set; }
+        public int ItemDEF { get; protected set; }
+        public int ItemHeal { get; protected set; }
+        public int ItemCount { get; protected set; }
+        protected string? ItemDescription;
+
+        public void AddCount()
+        {
+            ItemCount++;
+        }
+        public void RemoveCount()
+        {
+            if (ItemCount == 0) return;
+            ItemCount--;
+        }
+        public virtual string GetItemInfo(EItemDo eItemDo)
+        {
+            string itemInfo = "";
+
+            return itemInfo;
+        }
+    }
+
+    class EquipmentItem : Item
+    {
+
+        public EJob eJob { get; private set; }
+        protected bool IsArmor;
+        public bool IsPlayerEquip { get; set; }
+        public EquipmentItem(EItem eItem, EItemType eItemType, EJob eJob, int Price, int ItemATK, int ItemDEF, int ItemHeal, int ItemCount, bool IsArmor, bool IsPlayerEquip, string ItemDescription)
+        {
+            this.eItem = eItem;
+            this.eItemType = eItemType;
+            this.eJob = eJob;
+            this.Price = Price;
+            this.ItemATK = ItemATK;
+            this.ItemDEF = ItemDEF;
+            this.ItemHeal = ItemHeal;
+            this.ItemCount = ItemCount;
+            this.IsArmor = IsArmor;
+            this.IsPlayerEquip = IsPlayerEquip;
+            this.ItemDescription = ItemDescription;
+        }
+
+        public override string GetItemInfo(EItemDo eItemDo)
+        {
+            string iteminfo = "";
+
+            switch (eItemDo)
+            {
+                case EItemDo.ItemBuy:
+                    iteminfo =
+                          (IsPlayerEquip ? "[E] " : "")
+                        + ($"{eItem}")
+                        + (ItemATK != 0 ? $"| 공격력 + {ItemATK}" : "")
+                        + (ItemDEF != 0 ? $"| 방어력 + {ItemDEF}" : "")
+                        + (ItemHeal != 0 ? $"| 회복력 + {ItemHeal}" : "")
+                        + $"| {eJob}"
+                        + $"| {ItemDescription}"
+                        + $"| {Price} G";
+                    break;
+                case EItemDo.ItemSell:
+                    iteminfo =
+                          (IsPlayerEquip ? "[E] " : "")
+                        + ($"{eItem}")
+                        + (ItemATK != 0 ? $"| 공격력 + {ItemATK}" : "")
+                        + (ItemDEF != 0 ? $"| 방어력 + {ItemDEF}" : "")
+                        + (ItemHeal != 0 ? $"| 회복력 + {ItemHeal}" : "")
+                        + $"| {eJob}"
+                        + $"| {ItemDescription}"
+                        + $"| {Price * 85 / 100} G"
+                        + $"| x {ItemCount}";
+                    break;
+                case EItemDo.ItemView:
+                    iteminfo =
+                          (IsPlayerEquip ? "[E] " : "")
+                        + ($"{eItem}")
+                        + (ItemATK != 0 ? $"| 공격력 + {ItemATK}" : "")
+                        + (ItemDEF != 0 ? $"| 방어력 + {ItemDEF}" : "")
+                        + (ItemHeal != 0 ? $"| 회복력 + {ItemHeal}" : "")
+                        + $"| {eJob}"
+                        + $"| {ItemDescription}"
+                        + $"| x {ItemCount}";
+                    break;
+            }
+
+            return iteminfo;
+        }
+    }
+
+    class ConsumableItem : Item
+    {
+        public ConsumableItem(EItem eItem, EItemType eItemType, int Price, int ItemATK, int ItemDEF, int ItemHeal, int ItemCount, string ItemDescription)
+        {
+            this.eItem = eItem;
+            this.eItemType = eItemType;
+            this.Price = Price;
+            this.ItemATK = ItemATK;
+            this.ItemDEF = ItemDEF;
+            this.ItemHeal = ItemHeal;
+            this.ItemCount = ItemCount;
+            this.ItemDescription = ItemDescription;
+        }
+
+        public override string GetItemInfo(EItemDo eItemDo)
+        {
+            string iteminfo = "";
+
+            switch (eItemDo)
+            {
+                case EItemDo.ItemBuy:
+                    iteminfo =
+                        ($"{eItem}")
+                        + (ItemATK != 0 ? $"| 공격력 + {ItemATK}" : "")
+                        + (ItemDEF != 0 ? $"| 방어력 + {ItemDEF}" : "")
+                        + (ItemHeal != 0 ? $"| 회복력 + {ItemHeal}" : "")
+                        + $"| {ItemDescription}"
+                        + $"| {Price} G";
+                    break;
+                case EItemDo.ItemSell:
+                    iteminfo =
+                        ($"{eItem}")
+                        + (ItemATK != 0 ? $"| 공격력 + {ItemATK}" : "")
+                        + (ItemDEF != 0 ? $"| 방어력 + {ItemDEF}" : "")
+                        + (ItemHeal != 0 ? $"| 회복력 + {ItemHeal}" : "")
+                        + $"| {ItemDescription}"
+                        + $"| {Price * 85 / 100} G"
+                        + $"| x {ItemCount}";
+                    break;
+                case EItemDo.ItemView:
+                    iteminfo =
+                        ($"{eItem}")
+                        + (ItemATK != 0 ? $"| 공격력 + {ItemATK}" : "")
+                        + (ItemDEF != 0 ? $"| 방어력 + {ItemDEF}" : "")
+                        + (ItemHeal != 0 ? $"| 회복력 + {ItemHeal}" : "")
+                        + $"| {ItemDescription}"
+                        + $"| x {ItemCount}";
+                    break;
+            }
+
+            return iteminfo;
+        }
+    }
+}
